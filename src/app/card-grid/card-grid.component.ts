a0916aee049726fa9bcb8effba9877fa5433a8f9
ahmed66 <ahmedmeshaal66@gmail.com>
2022-10-03 09:51:36 +0300

    Design of Products List Section

---
+++ src/app/card-grid/card-grid.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-grid',
+  templateUrl: './card-grid.component.html',
+  styleUrls: ['./card-grid.component.css']
+})
+export class CardGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ImagePathProduct1 = "/assets/images/CT100410-CARHT_sm.jpeg";
+  ImagePathProduct2 = "/assets/images/CTJ131-CARBN_sm.jpeg";
+  ImagePathProduct3 = "/assets/images/CTJ162-OLIVE_sm.jpeg";
+  ImagePathProduct4 = "/assets/images/CTR41-CARBN_sm.jpeg";
+  AdImage = "/assets/images/ad-img.jpg";
+  ImagePathProduct5 = "/assets/images/GEP002-01_sm.jpeg";
+  ImagePathProduct6 = "/assets/images/GEP525-08_sm.jpeg";
+  ImagePathProduct7 = "/assets/images/GET018-01_sm.jpeg";
+  ImagePathProduct8 = "/assets/images/GET042-23_sm.jpeg";
+  AdImage2 = "/assets/images/871-3.jpg";
+  ImagePathProduct9 = "/assets/images/2245-CEIL_sm.jpeg";
+  ImagePathProduct10 = "/assets/images/2278-CARIB_sm.jpeg";
+  ImagePathProduct11 = "/assets/images/9133-TRUGP_sm.jpeg";
+  ImagePathProduct12 = "/assets/images/9233-NAVY_sm.jpeg";
+  AdImage3 = "/assets/images/ad-imagwe.jpg";
+  ImagePathProduct13 = "/assets/images/5555_HUNT_sm.jpeg";
+  ImagePathProduct14 = "/assets/images/9233-NAVY_sm (1).jpeg";
+  ImagePathProduct15 = "/assets/images/BOP513-65_sm.jpeg";
+  ImagePathProduct16 = "/assets/images/GRSP537-01_sm.jpeg";
+}
